f4386c5a273f96161546083c657edc14d1bf2890
Nicholas Peretti <nicholas@perettidesign.com>
2015-02-05 19:03:04 -0500

    2nd commit maybe

--- RITVender/ViewController.swift
+++ RITVender/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cameraView: UIView!
+    let captureSession: AVCaptureSession = AVCaptureSession();
+    var captureDevice : AVCaptureDevice?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        captureSession.sessionPreset = AVCaptureSessionPreset1280x720
+        
+        let devices = AVCaptureDevice.devices()
+        
+        for device in devices {
+
+            if (device.hasMediaType(AVMediaTypeVideo)) {
+
+                if(device.position == AVCaptureDevicePosition.Back) {
+                    captureDevice = device as? AVCaptureDevice
+                    if let videoformats = captureDevice?.formats {
+                        for captureFormat in videoformats{
+                            
+                        }
+                    }
+                }
+            }
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
