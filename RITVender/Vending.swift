4b920320dd9fdc36e899ba3bb123065622e5ce91
Lynne Meehan <ltmeehan@Lynne-Meehans-MacBook-Pro-2.local>
2015-02-05 19:20:46 -0500

    Hi Nick

---
+++ RITVender/Vending.swift
@@ -0,0 +1,15 @@
+//
+//  Vending.swift
+//  RITVender
+//
+//  Created by Lynne Meehan on 2/5/15.
+//  Copyright (c) 2015 Big Window Studios. All rights reserved.
+//
+
+import Foundation
+
+class Vending{
+    func VendingData: Dictionary{
+        
+    }
+}
